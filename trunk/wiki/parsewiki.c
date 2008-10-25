73888cbf3e68fc3a066735112f849dc7d5e49f08
svncode <svncode@16a6eac2-a0cf-11dd-a874-8d3a81574e67>
2008-10-25 05:57:11 +0000

    parsewiki

---
+++ trunk/wiki/parsewiki.c
@@ -0,0 +1,31 @@
+#include	<stdio.h>
+#include	<string.h>
+
+#define	MAXBUFFER	8192
+
+int main(int argc, char *argv[])
+{
+	char	buff[MAXBUFFER];
+	char	out[MAXBUFFER];
+
+	memset(buff, 0, sizeof(char) * MAXBUFFER);
+	while(fgets(buff, MAXBUFFER, stdin)){
+		char* p1 = buff;
+		char* p2;
+		do{
+			p1 = strstr(p1, "[[");
+			if(!p1)
+				break;
+			p2 = strstr(p1, "]]");
+			if(p1 && p2){
+				memset(out, 0, sizeof(char) * MAXBUFFER);
+				p1 += strlen("[[");
+				strncpy(out, p1 , p2 - p1 );
+				puts(out);
+				p1 = p2;
+			}
+		}while(p1 && p2);
+		memset(buff, 0, sizeof(char) * MAXBUFFER);
+	}
+	return 0;
+}
